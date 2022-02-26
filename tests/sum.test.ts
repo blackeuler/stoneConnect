6b206db23846a8251cde36111ac3ecbfc42c3bb9
Christopher Miller <miller1997@gmail.com>
2022-02-25 23:53:18 -0500

    fix

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -2,7 +2,3 @@ import { sum } from "../src/ts/index";
 test("Adds 1  + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
-
-test("Adds 1  + 2 to equal 4", () => {
-  expect(sum(1, 2)).toBe(4);
-});

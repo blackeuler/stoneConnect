58ead6e69e432ff5ff9e20d94f393e1fe9b8ac7b
Christopher Miller <miller1997@gmail.com>
2022-02-25 23:46:34 -0500

    Add Failing test to see if it fails CI

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -2,3 +2,7 @@ import { sum } from "../src/ts/index";
 test("Adds 1  + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("Adds 1  + 2 to equal 4", () => {
+  expect(sum(1, 2)).toBe(4);
+});
